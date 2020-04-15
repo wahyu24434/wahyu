6ca85ab166e0315ae82c5f7c738ba12444473be7
wahyu24434 <63726819+wahyu24434@users.noreply.github.com>
2020-04-15 15:12:20 +0100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=7muoUlkg6Jqs2yXuW2gxGsTRiBSaXYdmN2on9BeC";
         
         $("#loadImage").click(function(){
             $.ajax({
